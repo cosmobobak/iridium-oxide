505c4ac6196a24594856f346cbd7439dbc33eb84
cosmobobak <cosmobobak@gmail.com>
2022-02-27 14:12:04 +0000

    constant interdependence

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,5 @@
 pub const WIN_SCORE: i32 = 10;
-pub const DRAW_SCORE: i32 = 5;
+pub const DRAW_SCORE: i32 = WIN_SCORE / 2;
 
 pub const TREE_PRINT_DEPTH: usize = 2;
 pub const MAX_NODEPOOL_SIZE: usize = 50_000_000; // convert to use memory limit at some point
@@ -8,5 +8,5 @@ pub const ROOT_IDX: usize = 0;
 pub const INF: i32 = i32::max_value();
 pub const N_INF: i32 = -INF;
 
-pub const EXPLORATION_FACTOR: f64 = 10.0;
+pub const EXPLORATION_FACTOR: f64 = 1.0 * WIN_SCORE as f64;
 pub const NODE_UNVISITED_VALUE: f64 = f64::MAX;
